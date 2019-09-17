01e543853d0ccfca048b3dd28bc4c1f666c81d14
Gexygamma <gex98f@gmail.com>
2019-09-17 15:15:17 -0300

    Revert fitness function

--- TP3/TP3_AG.py
+++ TP3/TP3_AG.py
@@ -1,36 +1,42 @@
+print("Importando librerias...")
+print(" datetime...")
 import datetime
+print(" random...")
 import random as rnd
+print(" os...")
 import os
+print(" pandas...")
 import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
+print("Hecho!")
  
 # Crear lista de capitales
 nombresCapital = [
-    "Ciudad de Buenos Aires",
-    "Córdoba",
-    "Corrientes",
-    "Formosa",
-    "La Plata",
-    "La Rioja",
-    "Mendoza",
-    "Neuquen",
-    "Paraná",
-    "Posadas",
-    "Rawson",
-    "Resistencia",
-    "Río Gallegos",
-    "San Fernando del Valle de Catamarca",
-    "San Miguel de Tucumán",
-    "San Salvador de Jujuy",
-    "Salta",
-    "San Juan",
-    "San Luis",
-    "Santa Fe",
-    "Santa Rosa",
-    "Santiago del Estero",
-    "Ushuaia",
-    "Viedma" ]
+	"Ciudad de Buenos Aires",
+	"Córdoba",
+	"Corrientes",
+	"Formosa",
+	"La Plata",
+	"La Rioja",
+	"Mendoza",
+	"Neuquen",
+	"Paraná",
+	"Posadas",
+	"Rawson",
+	"Resistencia",
+	"Río Gallegos",
+	"San Fernando del Valle de Catamarca",
+	"San Miguel de Tucumán",
+	"San Salvador de Jujuy",
+	"Salta",
+	"San Juan",
+	"San Luis",
+	"Santa Fe",
+	"Santa Rosa",
+	"Santiago del Estero",
+	"Ushuaia",
+	"Viedma" ]
 
 # Inicializar parámetros de ejecución
 popSize = 50
@@ -39,215 +45,215 @@ probCrossover = 0.75
 probMutacion = 0.1
 number_list = list(range(chromSize))
 distance_matrix = []
- 
+
 def clearScreen():
-    # Limpia la terminal
-    os.system('cls' if os.name == 'nt' else 'clear')
+	# Limpia la terminal
+	os.system('cls' if os.name == 'nt' else 'clear')
  
 def decision(probability):
-    # Toma una decision aleatoria en base a una probabilidad
-    return rnd.random() < probability
+	# Toma una decision aleatoria en base a una probabilidad
+	return rnd.random() < probability
  
 def loadMatrix():
-    # Carga matriz de distancias en memoria desde un archivo xlsx
-    try: 
-        data = pd.read_excel('TP3/TablaCapitales.xlsx')
-    except FileNotFoundError:
-        data = pd.read_excel('TablaCapitales.xlsx')
-    for i in range(chromSize):
-        distance_matrix.append([])
-        for j in range(chromSize):
-            distance_matrix[i].append(data.iloc[i, j+1])
+	# Carga matriz de distancias en memoria desde un archivo xlsx
+	try: 
+		data = pd.read_excel('TP3/TablaCapitales.xlsx')
+	except FileNotFoundError:
+		data = pd.read_excel('TablaCapitales.xlsx')
+	for i in range(chromSize):
+		distance_matrix.append([])
+		for j in range(chromSize):
+			distance_matrix[i].append(data.iloc[i, j+1])
 
 def distance(capital1, capital2):
-    # Calcula distancia entre dos capitales
-    return distance_matrix[capital1][capital2]
+	# Calcula distancia entre dos capitales
+	return distance_matrix[capital1][capital2]
 
 def nearestCapital(recorrido):
-    capital = recorrido[-1]
-    distanciaMinima = -1
-    for i in range(chromSize):
-    	if (not i in recorrido) & ((distance(i, capital) < distanciaMinima) | (distanciaMinima == -1)):
-            distanciaMinima = distance(i, capital)
-            closest = i
-    return closest
- 
+	capital = recorrido[-1]
+	distanciaMinima = -1
+	for i in range(chromSize):
+		if (not i in recorrido) & ((distance(i, capital) < distanciaMinima) | (distanciaMinima == -1)):
+			distanciaMinima = distance(i, capital)
+			closest = i
+	return closest
+
 class Chromosome(object):
  
-    def __init__(self, size=None, madre=None, padre=None, punto=None):
-        # Constructor del cromosoma
-        self.gen = []
-        if size == None:
-        	pass
-        elif madre == None:
-            self.gen = rnd.sample(number_list, size)
-            self.calculateScore()
-        elif padre != None:
-            index = 0
-            self.gen = [None]*size
-            while(True):
-                self.gen[index] = madre.gen[index]
-                index = madre.gen.index(padre.gen[index])
-                if (index == 0):
-                    break
-            for i in range(size):
-                if (self.gen[i] is None):
-                    self.gen[i] = padre.gen[i]
-        else:
-            for i in range(size):
-                self.gen.append(madre.gen[i])
-            self.score=madre.score
+	def __init__(self, size=None, madre=None, padre=None, punto=None):
+		# Constructor del cromosoma
+		self.gen = []
+		if size == None:
+			pass
+		elif madre == None:
+			self.gen = rnd.sample(number_list, size)
+			self.calculateScore()
+		elif padre != None:
+			index = 0
+			self.gen = [None]*size
+			while(True):
+				self.gen[index] = madre.gen[index]
+				index = madre.gen.index(padre.gen[index])
+				if (index == 0):
+					break
+			for i in range(size):
+				if (self.gen[i] is None):
+					self.gen[i] = padre.gen[i]
+		else:
+			for i in range(size):
+				self.gen.append(madre.gen[i])
+			self.score=madre.score
  
-    def asString(self):
-        # Devuelve un string en base al arreglo de genes
-        string = []
-        for i in range(24):
-            string.append(str(self.gen[i]))
-        return ",".join(string)
+	def asString(self):
+		# Devuelve un string en base al arreglo de genes
+		string = []
+		for i in range(24):
+			string.append(str(self.gen[i]))
+		return ",".join(string)
  
-    def calculateScore(self):
-        # Calcula y guarda el puntaje, que representa la distancia recorrida
-        self.score = 0
-        for i in range(len(self.gen)-1):
-            self.score += distance(self.gen[i], self.gen[i+1])
-        self.score += distance(self.gen[-1], self.gen[0])
+	def calculateScore(self):
+		# Calcula y guarda el puntaje, que representa la distancia recorrida
+		self.score = 0
+		for i in range(len(self.gen)-1):
+			self.score += distance(self.gen[i], self.gen[i+1])
+		self.score += distance(self.gen[-1], self.gen[0])
  
-    def calculateFitness(self, totalScore):
-        # Calcula y guarda el valor fitness
-        self.fitness = 1 - self.score/totalScore
+	def calculateFitness(self, totalScore):
+		# Calcula y guarda el valor fitness
+		self.fitness = totalScore/self.score
  
-    def mutate(self):
-        # Muta al cromosoma, intercambiando dos genes aleatorios de lugar
-        points = rnd.sample(number_list, k=2)
-        cap1 = self.gen[points[0]]
-        cap2 = self.gen[points[1]]
-        self.gen[points[0]] = cap2
-        self.gen[points[1]] = cap1
-        self.calculateScore()
-     
+	def mutate(self):
+		# Muta al cromosoma, intercambiando dos genes aleatorios de lugar
+		points = rnd.sample(number_list, k=2)
+		cap1 = self.gen[points[0]]
+		cap2 = self.gen[points[1]]
+		self.gen[points[0]] = cap2
+		self.gen[points[1]] = cap1
+		self.calculateScore()
+	 
 class Population(object):
  
-    def __init__(self, prevPopulation=None):
-        # Constructor de la población
-        self.chromosomes = []
-        self.totalScore = 0
-        # Crear población aleatoriamente
-        if prevPopulation == None:
-            for i in range(popSize):
-                individuo = Chromosome(chromSize)
-                self.addChromosome(individuo)
-        # Crear población hija en base a la población previa
-        else:
-            # Aplicar elitismo
-            self.addChromosome(Chromosome(chromSize, prevPopulation.chromosomes[-2]))
-            self.addChromosome(Chromosome(chromSize, prevPopulation.chromosomes[-1]))
-            for i in range(int(popSize/2)-1):
-                padre = prevPopulation.selectWeightedChromosome()
-                madre = prevPopulation.selectWeightedChromosome()
-                # Aplicar crossover
-                if decision(probCrossover) & (padre!=madre):
-                    hijo1 = Chromosome(chromSize, madre, padre)
-                    hijo2 = Chromosome(chromSize, padre, madre)
-                    isNew = True
-                else:
-                    hijo1 = Chromosome(chromSize, padre)
-                    hijo2 = Chromosome(chromSize, madre)
-                    isNew = False
-                # Aplicar mutación
-                if decision(probMutacion):
-                    hijo1.mutate()
-                elif isNew:
-                    hijo1.calculateScore()
-                if decision(probMutacion):
-                    hijo2.mutate()
-                elif isNew:
-                    hijo2.calculateScore()
-                # Agregar cromosomas hijos
-                self.addChromosome(hijo1)
-                self.addChromosome(hijo2)
+	def __init__(self, prevPopulation=None):
+		# Constructor de la población
+		self.chromosomes = []
+		self.totalScore = 0
+		# Crear población aleatoriamente
+		if prevPopulation == None:
+			for i in range(popSize):
+				individuo = Chromosome(chromSize)
+				self.addChromosome(individuo)
+		# Crear población hija en base a la población previa
+		else:
+			# Aplicar elitismo
+			self.addChromosome(Chromosome(chromSize, prevPopulation.chromosomes[-2]))
+			self.addChromosome(Chromosome(chromSize, prevPopulation.chromosomes[-1]))
+			for i in range(int(popSize/2)-1):
+				padre = prevPopulation.selectWeightedChromosome()
+				madre = prevPopulation.selectWeightedChromosome()
+				# Aplicar crossover
+				if decision(probCrossover) & (padre!=madre):
+					hijo1 = Chromosome(chromSize, madre, padre)
+					hijo2 = Chromosome(chromSize, padre, madre)
+					isNew = True
+				else:
+					hijo1 = Chromosome(chromSize, padre)
+					hijo2 = Chromosome(chromSize, madre)
+					isNew = False
+				# Aplicar mutación
+				if decision(probMutacion):
+					hijo1.mutate()
+				elif isNew:
+					hijo1.calculateScore()
+				if decision(probMutacion):
+					hijo2.mutate()
+				elif isNew:
+					hijo2.calculateScore()
+				# Agregar cromosomas hijos
+				self.addChromosome(hijo1)
+				self.addChromosome(hijo2)
  
-    def addChromosome(self, c):
-        # Agrega un cromosoma a la población
-        self.chromosomes.append(c)
+	def addChromosome(self, c):
+		# Agrega un cromosoma a la población
+		self.chromosomes.append(c)
  
-    def calculateTotalScore(self):
-        # Calcula y guarda la suma de los puntajes de todos los cromosomas
-        for c in self.chromosomes:
-            self.totalScore += c.score
+	def calculateTotalScore(self):
+		# Calcula y guarda la suma de los puntajes de todos los cromosomas
+		for c in self.chromosomes:
+			self.totalScore += c.score
  
-    def sortByFitness(self):
-        # Calcula los valores fitness de cada cromosoma y ordena de menor a mayor
-        for c in self.chromosomes:
-            c.calculateFitness(self.totalScore)
-        self.chromosomes.sort(key = lambda Chromosome: Chromosome.fitness)
+	def sortByFitness(self):
+		# Calcula los valores fitness de cada cromosoma y ordena de menor a mayor
+		for c in self.chromosomes:
+			c.calculateFitness(self.totalScore)
+		self.chromosomes.sort(key = lambda Chromosome: Chromosome.fitness)
  
-    def selectWeightedChromosome(self):
-        # Devuelve un cromosoma, teniendo mayor probabilidad dependiendo de su valor fitness
-        # Obtener lista de los valores fitness de los cromosomas
-        w = []
-        for c in self.chromosomes:
-            w.append(c.fitness)
-        # Devolver cromosoma aleatorio
-        return rnd.choices(
-            population = self.chromosomes,
-            weights = w
-        )[0]
+	def selectWeightedChromosome(self):
+		# Devuelve un cromosoma, teniendo mayor probabilidad dependiendo de su valor fitness
+		# Obtener lista de los valores fitness de los cromosomas
+		w = []
+		for c in self.chromosomes:
+			w.append(c.fitness)
+		# Devolver cromosoma aleatorio
+		return rnd.choices(
+			population = self.chromosomes,
+			weights = w
+		)[0]
  
-    def printStats(self):
-        # Imprime en pantalla estadisticas de la población
-        minimo = self.chromosomes[0].score
-        maximo = self.chromosomes[-1].score # Indice -1 devuelve el último item
-        promedio = self.totalScore / len(self.chromosomes)
-        print("Máximo: ", maximo, "Mínimo: ", minimo, "Promedio: ", promedio)
+	def printStats(self):
+		# Imprime en pantalla estadisticas de la población
+		minimo = self.chromosomes[0].score
+		maximo = self.chromosomes[-1].score # Indice -1 devuelve el último item
+		promedio = self.totalScore / len(self.chromosomes)
+		print("Máximo: ", maximo, "Mínimo: ", minimo, "Promedio: ", promedio)
  
-    def printChrom(self):
-        # Imprime en pantalla la lista de cromosomas
-        for c in self.chromosomes:
-            print(c.asString(), " f:", c.fitness)
+	def printChrom(self):
+		# Imprime en pantalla la lista de cromosomas
+		for c in self.chromosomes:
+			print(c.asString(), " f:", c.fitness)
 
 
 def heuristica(capitalInicial):
-    result = Chromosome()
-    result.gen.append(capitalInicial)
-    while (True):
-        result.gen.append(nearestCapital(result.gen))
-        if len(result.gen) == 24:
-        	result.calculateScore()
-        	break
-    return result
+	result = Chromosome()
+	result.gen.append(capitalInicial)
+	while (True):
+		result.gen.append(nearestCapital(result.gen))
+		if len(result.gen) == 24:
+			result.calculateScore()
+			break
+	return result
 
 def genetico():
 	# Obtener número de iteraciones del usuario
 	iteraciones = int(input("Ingrese cantidad de iteraciones a ejecutar: "))
-	 
+	
 	# Crear primera instancia de la población
 	newPopulation = Population()
 	 
 	for n in range(iteraciones):
-	    # Calcular y guardar puntajes y valores fitness de los cromosomas
-	    newPopulation.calculateTotalScore()
-	    newPopulation.sortByFitness()
-	    # Crear nueva población en base a la anterior
-	    prevPopulation = newPopulation
-	    newPopulation = Population(prevPopulation)
+		# Calcular y guardar puntajes y valores fitness de los cromosomas
+		newPopulation.calculateTotalScore()
+		newPopulation.sortByFitness()
+		# Crear nueva población en base a la anterior
+		prevPopulation = newPopulation
+		newPopulation = Population(prevPopulation)
 	 
 	resultChrom = prevPopulation.chromosomes[-1]
 	
 	# Imprimir resultado final en pantalla
 	print("Resultado\n Cromosoma: {chrom}\n Objetivo: {score}\n Fitness: {fitness}"
-	    .format(
-	       chrom = resultChrom.asString(),
-	       score = resultChrom.score,
-	       fitness = resultChrom.fitness))
+		.format(
+		   chrom = resultChrom.asString(),
+		   score = resultChrom.score,
+		   fitness = resultChrom.fitness))
 	input("Presione una tecla para volver al menú...")
 
 def busquedaUnitaria():
 	capital = int(input("Ingrese la capital de partida: "))
 	resultChrom = heuristica(capital)
 	print("Resultado\n Recorrido: {chrom}\n Objetivo: {score}\n"
-	    .format(
-	       chrom = resultChrom.asString(),
-	       score = resultChrom.score))
+		.format(
+		   chrom = resultChrom.asString(),
+		   score = resultChrom.score))
 	input("Presione una tecla para volver al menú...")
 
 def busquedaTotal():
@@ -258,19 +264,19 @@ def busquedaTotal():
 			bestScore = resultChrom.score
 			best = resultChrom
 	print("Resultado\n Recorrido: {chrom}\n Objetivo: {score}\n"
-	    .format(
-	       chrom = best.asString(),
-	       score = best.score))
+		.format(
+		   chrom = best.asString(),
+		   score = best.score))
 	input("Presione una tecla para volver al menú...")
 	
- 
 #################################
 # INICIO DEL PROGRAMA PRINCIPAL #
 #################################
 
-# Carga matriz en memoria
+# Cargar matriz en memoria
 loadMatrix()
 
+# Mostrar menú del programa
 opcion = 1
 while (opcion != 0):
 	clearScreen()
